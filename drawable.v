d433c42ef3a1907e9ed6ed4688682b91786d7a95
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-03-25 21:32:57 -0400

    Updated the boundaries code

--- drawable.v
+++ drawable.v
@@ -73,11 +73,13 @@ module drawable(clock, enable, reset_n, height, width, x_pos, y_pos, colour, x_o
 	 
 endmodule
 
-module boundaries(clock, enable, reset_n, x_out, y_out, colour_out, done);
-	input clock, enable, reset_n;
+module boundaries(clock,reset_n, x_out, y_out, colour_out, writeEn);
+	input clock, reset_n;
+	wire enable;
 	output [7:0] x_out;
 	output [6:0] y_out;
 	output [2:0] colour_out;
+	output writeEn;
 	
 	wire [7:0] width;
 	wire [6:0] height;
@@ -85,27 +87,30 @@ module boundaries(clock, enable, reset_n, x_out, y_out, colour_out, done);
 	wire [6:0] y_pos;
 	
 	wire draw_done;
+	wire enable_fcounter;
+	wire reset_draw;
+	
+	control_boundaries cb0(.reset_draw(reset_draw), .clock(clock), .reset_n(reset_n), .width(width), .height(height), .x_pos(x_pos), .y_pos(y_pos), .draw_done(draw_done), .enable(enable), .writeEn(writeEn));
 	
 	drawable top(
-	.clock(clock), .enable(enable), .reset_n(reset_n), 
-	.height(7'd4), .width(8'd50), 
-	.x_pos(8'd0), .y_pos(7'd0), .colour(3'b010), 
+	.clock(clock), .enable(enable), .reset_n(reset_draw), 
+	.height(height), .width(width), 
+	.x_pos(x_pos), .y_pos(y_pos), .colour(3'b010), 
 	.x_out(x_out), .y_out(y_out), .colour_out(colour_out), 
 	.enable_fcounter(enable_fcounter), .y_count_done(draw_done));
 	
-	control_boundaries adam_pussy(.clock(clock), .reset_n(reset_n), .width(width), .height(height), .x_pos(x_pos), .y_pos(y_pos), .draw_done(draw_done))
+	
 
 endmodule
 
-module control_boundaries(clock, reset_n, width, height, x_pos, y_pos, draw_done);
-	 input clock, reset_n,;
+module control_boundaries(clock, reset_n, width, height, x_pos, y_pos, draw_done, enable, writeEn, reset_draw);
+	 input clock, reset_n;
+	 output reg enable, writeEn, reset_draw;
 	 output reg [7:0] width;
 	 output reg [6:0] height;
 	 output reg [7:0] x_pos;
 	 output reg [6:0] y_pos;
-	 output enable_drawable;
-	 
-	 input reg draw_done;
+	 input draw_done;
 	 
 	 reg [3:0] curr_state, next_state;
 	 
@@ -113,20 +118,26 @@ module control_boundaries(clock, reset_n, width, height, x_pos, y_pos, draw_done
                 BOTTOM   = 4'd1,
                 TOP_LEFT        = 4'd2,
                 BOTTOM_LEFT   = 4'd3,
-					 TOP_RIGHT = 4'd4;
+					 TOP_RIGHT = 4'd4,
 					 BOTTOM_RIGHT = 4'd5;
 					 
 	// Next state logic aka our stRESET_COUNTERate table
 	
 	//CONSIDER WAITING STATES
     always@(*)
-    begin: state_table y_count_done
+    begin: state_table
             case (curr_state)
                 TOP: next_state = (draw_done == 1) ? BOTTOM: TOP; 
                 BOTTOM: next_state = (draw_done == 1) ? TOP_LEFT : BOTTOM;
                 TOP_LEFT: next_state  = (draw_done == 1) ? BOTTOM_LEFT: TOP_LEFT;
                 BOTTOM_LEFT: next_state = (draw_done == 1) ? TOP_RIGHT: BOTTOM_LEFT;
-					 TOP_RIGHT = next_state = (draw_done == 1) ? BOTTOM_RIGHT : TOP_RIGHT;
+					 TOP_RIGHT: next_state = (draw_done == 1) ? BOTTOM_RIGHT : TOP_RIGHT;
+//					 TOP: next_state = BOTTOM; 
+//              BOTTOM: next_state = TOP_LEFT ;
+//              TOP_LEFT: next_state  =  BOTTOM_LEFT;
+//              BOTTOM_LEFT: next_state = TOP_RIGHT;
+//					 TOP_RIGHT: next_state = BOTTOM_RIGHT;
+//					 BOTTOM_RIGHT: next_state = TOP;
             default: next_state = TOP;
         endcase
     end // state_table
@@ -139,43 +150,71 @@ module control_boundaries(clock, reset_n, width, height, x_pos, y_pos, draw_done
 		  height <= 0;
 		  x_pos <= 0;
 		  y_pos <= 0;
-		
+		  enable <= 0;
+		  writeEn <= 0;
+		  reset_draw <= 1;
+		  
+		 
+
         case (curr_state)
             TOP: begin
-						width <= 100;
-						height <= 5;
+						width <= 4;
+						height <= 4;
 						x_pos <= 0;
 						y_pos <= 0;
+						enable <= 1'b1;
+						 writeEn <= 1;
+						 reset_draw <= 0;
                 end
 				BOTTOM: begin
-					   width <= 100;
-						height <= 5;
+					   width <= 4;
+						height <= 4;
 						x_pos <= 0;
-						y_pos <= 100;
+						y_pos <= 50;
+						enable <= 1'b1;
+						 writeEn <= 1;
+						 reset_draw <= 0;
+
                 end
             TOP_LEFT: begin
-                  width <= 5;
-						height <= 12;
-						x_pos <= 0;
-						y_pos <= 0;
+                  width <= 4;
+						height <= 4;
+						x_pos <= 50;
+						y_pos <= 50;
+						enable <= 1'b1;
+						 writeEn <= 1;
+						 reset_draw <= 0;
+
                 end
 				BOTTOM_LEFT: begin
-				      width <= 5;
-						height <= 12;
-						x_pos <= 0;
-						y_pos <= 100;
+				      width <= 4;
+						height <= 4;
+						x_pos <= 50;
+						y_pos <= 0;
+						enable <= 1'b1;
+						 writeEn <= 1;
+						 reset_draw <= 0;
+
                 end
 				TOP_RIGHT: begin
-					   width <= 5;
-						height <= 12;
-						x_pos <= 100;
-						y_pos <= 0;
+					   width <= 4;
+						height <= 4;
+						x_pos <= 20;
+						y_pos <= 30;
+						enable <= 1'b1;
+						 writeEn <= 1;
+						 reset_draw <= 0;
+	
                 end
 				BOTTOM_RIGHT: begin
-					   width <= 5;
-						height <= 12;
-						x_pos <= 100;
-						y_pos <= 100;
+					   width <= 4;
+						height <= 4;
+						x_pos <= 10;
+						y_pos <= 20;
+						enable <= 1'b1;
+						 writeEn <= 1;
+						 reset_draw <= 0;
+
                 end
         // default:    // don't need default since we already made sure all of our outputs were assigned a value at the start of the always block
         endcase
