0c81655a39782aa8a34f6f5e0ebde4d40112e5a1
HareshNasit <harsh.nasit@mail.utoronto.ca>
2019-03-27 19:00:47 -0400

    Updated the collision file to include paddle ball collision

--- collision.v
+++ collision.v
@@ -1,40 +1,60 @@
 
 
-module collision(clock, enable, reset_n, x, y, vertical, horizontal);
+module collision(clock, enable, reset_n, x_ball, y_ball, x_paddle1, y_paddle1, x_paddle2, y_paddle2, vertical, horizontal);
 	//x_in, y_in is top left pixel of the box
 	
 	input clock, enable, reset_n;
 	
-	input [7:0] x; 
-   input [6:0] y;
+	input [10:0] x_ball; 
+   input [10:0] y_ball;
+	input [10:0] x_paddle1, x_paddle2;
+	input [10:0] y_paddle1, y_paddle2;
 	
-	output reg vertical, horizontal;
+	output  vertical, horizontal;
+	reg vertical_inside, horizontal_inside;
 
 	always @(posedge clock)
 	begin
 		if (reset_n)begin
-			horizontal <= 0;
-			vertical <= 1;
+			horizontal_inside <= 0;
+			vertical_inside <= 1;
 		end
 		else if (enable) begin
 		
-			if (x == 7'b0000000) begin
-				horizontal <= 1;
+			if (x_ball == 7'b0000000) begin
+				horizontal_inside <= 1;
 			end 
-			else if (x + 4 == 100) begin
-				horizontal <= 0;
+			else if (x_ball + 4 == 100) begin
+				horizontal_inside <= 0;
 			end	
 			
-		  if (y == 6'b000000) begin
-				vertical <= 0;
+		  if (y_ball == 6'b000000) begin
+				vertical_inside <= 0;
 			end
-			else if (y + 4 == 100) begin
-				vertical <= 1;
+			else if (y_ball + 4 == 100) begin
+				vertical_inside <= 1;
 			end
-		
-		
 		end
+		if (x_paddle1 <= x_ball && x_ball <= (x_paddle1 + 4)) 
+		begin
+			if (y_paddle1 <= y_ball && y_ball <= (y_paddle1 + 40))
+			begin
+				vertical_inside <= ~vertical_inside;
+				horizontal_inside <= ~horizontal_inside;
+			end
+		end
+		if (x_paddle2 <= x_ball && x_ball <= (x_paddle2 + 4)) 
+		begin
+			if (y_paddle2 <= y_ball && y_ball <= (y_paddle2 + 40))
+			begin
+				vertical_inside <= ~vertical_inside;
+				horizontal_inside <= ~horizontal_inside;
+			end
+		end
+		
 	end
 
+	assign vertical = vertical_inside;
+	assign horizontal = horizontal_inside;
 
 endmodule
